4dddbad7c5dc9499673708c95f7d4d6db4c7cec7
itcongyidong1 <309568081@qq.com>
2019-05-04 15:09:49 +0800

    idea 第一次提交

---
+++ src/packageTest/test.java
@@ -0,0 +1,11 @@
+package packageTest;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.err.println(3);
+
+    }
+
+}
